ea63f655ae8c66b500fb24e072af04a0eea286ba
lukesmith24 <wellsvilleluke@gmail.com>
2025-09-24 12:49:13 -0500

    Create Chapter3.js

---
+++ Chapter3Project/Chapter3.js
@@ -0,0 +1,26 @@
+/*
+    Chapter 3 project
+    Author: Luke Smith
+    Date: 9/24/2025
+*/
+const fullName = "Luke Smith";
+console.log("fullName", fullName);
+
+const desiredSalary = 60000;
+console.log("desiredSalary:", desiredSalary);
+
+const isVeteran = false;
+console.log("isVeteran", isVeteran);
+
+const friendNames = ["Aidan", "Ethan", "Dylan"];
+console.log("friendNames", friendNames);
+
+const friendSalaries = [50000, 55000, 60000];
+console.log("friendSalaries", friendSalaries);
+
+const anotherFriend = {
+    firstName: "Jacob",
+    lastName: "Johnson",
+    desiredSalary: 65000
+};
+console.log("anotherFriend", anotherFriend);
